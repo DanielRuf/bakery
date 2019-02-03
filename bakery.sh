5f950f5eb1c7d5cc63e4814475eedcc2470c924b
Daniel Ruf <daniel@daniel-ruf.de>
2019-02-03 16:29:34 +0100

    Check if brotli is globally available and add brotli command

--- bakery.sh
+++ bakery.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v brotli)" ]; then
 BAKERY_DIRECTORY="bakery"
 BROTLI_DOWNLOAD="https://dl.bintray.com/eustas/brotli/brotli-linux-2018-11-12.zip"
 
-# create bakery directory if it does not exist
-if [ ! -d "$BAKERY_DIRECTORY" ]; then
-  mkdir $BAKERY_DIRECTORY
-fi
+  # create bakery directory if it does not exist
+  if [ ! -d "$BAKERY_DIRECTORY" ]; then
+    mkdir $BAKERY_DIRECTORY
+  fi
 
-# download brotli binary if it does not exist
-if [ ! -f "$BAKERY_DIRECTORY/brotli" ]; then
-  rm -f $BAKERY_DIRECTORY/*
-  curl -#SL $BROTLI_DOWNLOAD > brotli.zip
-  unzip brotli.zip -d $BAKERY_DIRECTORY
-  rm brotli.zip
+  # download brotli binary if it does not exist
+  if [ ! -f "$BAKERY_DIRECTORY/brotli" ]; then
+    rm -f $BAKERY_DIRECTORY/*
+    curl -#SL $BROTLI_DOWNLOAD > brotli.zip
+    unzip brotli.zip -d $BAKERY_DIRECTORY
+    rm brotli.zip
+  fi
+
 fi
 
 for f in $1/*.{css,js}
 do
   echo "Processing $f file..."
+  brotli -5 $f
 done
 
 echo "Done"
